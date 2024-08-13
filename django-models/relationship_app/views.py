3b24f60eaf0b99693db11405129311ef44bd6f83
Ezrah <ezrahpietersen309@gmail.com>
2024-08-12 23:53:51 -0700

    added file

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -14,3 +14,34 @@ class LibraryDetailView(DetailView):
 relationship_app/list_books.html", "Book.objects.all()
 relationship_app/library_detail.html
 from django.views.generic.detail import DetailView
+from django.shortcuts import render, redirect
+from django.contrib.auth import login as auth_login, logout as auth_logout
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+
+def login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            auth_login(request, user)
+            return redirect('home')  # Redirect to a home page or any other page
+    else:
+        form = AuthenticationForm()
+    return render(request, 'login.html', {'form': form})
+
+def logout(request):
+    auth_logout(request)
+    return render(request, 'logout.html')
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'register.html', {'form': form})
+from django.contrib.auth import login", "from django.contrib.auth.forms import UserCreationForm
+
