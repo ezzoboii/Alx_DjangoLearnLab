c5ec7dfff5289bd23f3cce486337e515f3f6fb61
Ezrah <ezrahpietersen309@gmail.com>
2024-08-25 04:46:59 -0700

    addedchanges

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,3 +1,22 @@
 class CustomUser(AbstractUser):", "date_of_birth", "profile_photo
 class CustomUserManager(BaseUserManager):", "create_user", "create_superuser
 class Book(models.Model):", "can_create", "can_delete
+from django.db import models
+from django.contrib.auth.models import User
+
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+    published_date = models.DateField()
+
+    class Meta:
+        permissions = [
+            ("can_view", "Can view book"),
+            ("can_create", "Can create book"),
+            ("can_edit", "Can edit book"),
+            ("can_delete", "Can delete book"),
+        ]
+
+    def __str__(self):
+        return self.title
+
