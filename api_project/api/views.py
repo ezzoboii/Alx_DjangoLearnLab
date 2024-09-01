1687539458c0d69a2d5c8a9cf5ad74bba672d769
Ezrah <ezrahpietersen309@gmail.com>
2024-09-01 14:45:20 -0700

    added new commands

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -6,4 +6,12 @@ from .serializers import BookSerializer
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+# api/views.py
+from rest_framework import viewsets
+from .models import Book
+from .serializers import BookSerializer
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
 
