f977324ec67d2975a469dc76e82a8f0d93cfbc74
Ezrah <ezrahpietersen309@gmail.com>
2024-09-01 14:46:33 -0700

    addedcommand

--- api_project/api/serializers.py
+++ api_project/api/serializers.py
@@ -6,4 +6,12 @@ class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
         fields = '__all__'  # Include all fields from the Book model
+# api/serializers.py
+from rest_framework import serializers
+from .models import Book
+
+class BookSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Book
+        fields = '__all__'
 
