0f7b8beb9bdcafc0768730d86e482b6de128f455
Ezrah <ezrahpietersen309@gmail.com>
2024-09-03 10:16:12 -0700

    added commands

--- advanced-api-project/advanced_api_project/api/models.py
+++ advanced-api-project/advanced_api_project/api/models.py
@@ -17,4 +17,21 @@ class Book(models.Model):
 
     def __str__(self):
         return self.title
+from django.db import models
+
+class Author(models.Model):
+    """Model representing an author."""
+    name = models.CharField(max_length=255)  # Author's name
+
+    def __str__(self):
+        return self.name
+
+class Book(models.Model):
+    """Model representing a book."""
+    title = models.CharField(max_length=255)  # Title of the book
+    publication_year = models.IntegerField()  # Year the book was published
+    author = models.ForeignKey(Author, related_name='books', on_delete=models.CASCADE)  # Link to the author
+
+    def __str__(self):
+        return self.title
 
--- advanced-api-project/advanced_api_project/api/serializers.py
+++ advanced-api-project/advanced_api_project/api/serializers.py
@@ -17,4 +17,26 @@ class AuthorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Author
         fields = ['name', 'books']
+from rest_framework import serializers
+from .models import Author, Book
+
+class BookSerializer(serializers.ModelSerializer):
+    """Serializer for the Book model."""
+    class Meta:
+        model = Book
+        fields = ['title', 'publication_year', 'author']
+    
+    def validate_publication_year(self, value):
+        """Ensure the publication year is not in the future."""
+        if value > datetime.date.today().year:
+            raise serializers.ValidationError("Publication year cannot be in the future.")
+        return value
+
+class AuthorSerializer(serializers.ModelSerializer):
+    """Serializer for the Author model, including nested BookSerializer."""
+    books = BookSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Author
+        fields = ['name', 'books']
 
