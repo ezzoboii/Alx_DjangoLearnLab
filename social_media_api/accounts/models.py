b62ecffcdd73b0041f80d2e8106b23e312976838
Ezrah <ezrahpietersen309@gmail.com>
2024-09-27 12:59:46 -0700

    added commands

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -1,1 +1,10 @@
 from django.contrib.auth.models import AbstractUser", "bio", "profile_picture", "followers", "models.ManyToManyField", "models.ImageField", "models.TextField
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+class User(AbstractUser):
+    following = models.ManyToManyField('self', symmetrical=False, related_name='followers')
+
+    def __str__(self):
+        return self.username
+
