d77500a8d526f03b82003bcf003b61a5b52f32b1
Ezrah <ezrahpietersen309@gmail.com>
2024-10-25 04:14:19 -0700

    changes made

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -45,3 +45,4 @@ def register(request):
     return render(request, 'register.html', {'form': form})
 from django.contrib.auth import login", "from django.contrib.auth.forms import UserCreationForm
 
+from django.contrib.auth.decorators import permission_required", "relationship_app.can_add_book", "relationship_app.can_change_book", "relationship_app.can_delete_book
